5a32ca550c6c02aee0c7e63df0a664c1cca00e16
Lydon-01 <lydoncarter@gmail.com>
2017-12-28 09:40:26 +0200

    Added Detect STREAM variable

--- kinesisStream_decodeRecord.sh
+++ kinesisStream_decodeRecord.sh
@@ -1,6 +1,9 @@
-ITERATOR=$(aws kinesis get-shard-iterator --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --stream-name TempStream2 | tr -d '{}" ' |sed "s/ShardIterator//g" )
-echo $ITERATOR
+STREAM=`aws kinesis list-streams | grep '        "'|tr -d '        "'`
+ITERATOR=$(aws kinesis get-shard-iterator --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --stream-name $STREAM | tr -d '{}" ' |sed "s/ShardIterator//g" )
+# echo $ITERATOR
+
 DATA=$(aws kinesis get-records --shard-iterator $ITERATOR | grep Data |  tr -d ' ",:' |sed "s/Data//g")
+
 for entry in $DATA
 do
 	echo -e $entry | base64 --decode
