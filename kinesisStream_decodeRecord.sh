92dc85533a3900a2b48663a8ea34615f031db7bf
Lydon-01 <lydoncarter@gmail.com>
2017-12-29 09:04:29 +0200

    Update kinesisStream_decodeRecord.sh

--- kinesisStream_decodeRecord.sh
+++ kinesisStream_decodeRecord.sh
@@ -8,7 +8,6 @@
 # Ask which Shard
 echo  "$(tput setaf 2)Which Shard are we checking out? Give just the last number (0-999)$(tput sgr0)"
 read SHARD_LAST_NUM
-echo $SHARD_LAST_NUM
 SHARD_ID=shardId-00000000000$SHARD_LAST_NUM # Correctly save the ShardID choice.
 echo  "$(tput setaf 2)Cool, checking out data on $SHARD_ID.$(tput sgr0)"
 echo ""
@@ -21,7 +20,7 @@ TYPE=TRIM_HORIZON
 STREAM=`aws kinesis list-streams | grep '        "'|tr -d '        "'`
 
 # Saves the Iterator Name of the shard
-ITERATOR=$(aws kinesis get-shard-iterator --shard-id shardId-000000000000 --shard-iterator-type $OPTION --stream-name $STREAM | tr -d '{}" ' |sed "s/ShardIterator//g" )
+ITERATOR=$(aws kinesis get-shard-iterator --shard-id shardId-000000000000 --shard-iterator-type $TYPE --stream-name $STREAM | tr -d '{}" ' |sed "s/ShardIterator//g" )
 # echo $ITERATOR
 
 DATA=$(aws kinesis get-records --shard-iterator $ITERATOR | grep Data |  tr -d ' ",:' |sed "s/Data//g")
