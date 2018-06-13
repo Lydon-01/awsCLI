3aeb20371d4ff0381e9adedc677841f66e7981bc
Lydon-01 <lydoncarter@gmail.com>
2018-06-13 10:31:17 +0200

    Added comma between words
    In order to create CSV format, added comma between the RandomWords

--- kinesisStream_randomPuts.sh
+++ kinesisStream_randomPuts.sh
@@ -2,8 +2,8 @@
 ## This script will create random data into a Kinesis stream
 ## Still To do: 
 #1 Fix Word3 to have a bigger data entry
-#2 Better understand the partition key and how to use it correctly
-#3 
+#2 Have the entries written in a format such as CSV or JSON
+#3 Better understand the partition key and how to use it correctly
 
 # Function for the randomWord
 randomWord(){
@@ -25,8 +25,8 @@ echo ""
 N=1
 while [ $N -le $COUNT ]
 do
-        WORD1=$(randomWord)
-        WORD2=$(randomWord) # Using two word so that you can play with different partition keys
+        WORD1=$(randomWord)","
+        WORD2=$(randomWord)"," # Using two word so that you can play with different partition keys
         ## Word3 feature coming soon.
         #WORD3="$(randomWord) $(randomWord) $(randomWord)  $(randomWord) $(randomWord) $(randomWord)"
 
