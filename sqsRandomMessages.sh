d61cfff03a343b004a2bc1b9196ab4f37f22d32e
Lydon-01 <lydoncarter@gmail.com>
2019-12-27 09:37:27 +0200

    Fixed cat command
    Rather used a $word variable

--- sqsRandomMessages.sh
+++ sqsRandomMessages.sh
@@ -1,4 +1,4 @@
 ## Simple one-liner bash script to put messages into a SQS queue
 ## Messages will be in CSV format with an itirating id, random word and epoc time. 
 
-for i in $(seq 1 1000); do aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/YOURACCOUNTID/YOURQUEUE --message-body $i,$(cat /usr/share/dict/words|sort -R|head -n 1|xargs echo),$(date +%s); done;
+for i in $(seq 1 100); do word=$(cat /usr/share/dict/words|sort -R|head -n 1|xargs echo); aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/076590427499/test --message-body "$i,$word,$(date +%s)"; done;
