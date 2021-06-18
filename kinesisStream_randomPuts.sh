f1f37881890874e3af6651398c97dc44c06e041f
Lydon-01 <lydoncarter@gmail.com>
2021-06-18 15:57:36 +0200

    Proved output

--- kinesisStream_randomPuts.sh
+++ kinesisStream_randomPuts.sh
@@ -16,7 +16,7 @@ STREAM=`aws kinesis list-streams | grep '        "'|tr -d '        "'`
 
 # How many puts? 
 echo ""
-echo "$(tput setaf 2)Yo! How many Put-Records would you like to do per partiton-key?$(tput sgr0)"
+echo "$(tput setaf 2) How many Put-Records would you like to \do?$(tput sgr0)"
 read COUNT
 echo "$(tput setaf 2)Okay. Doing $COUNT Put-Records into Stream $STREAM.$(tput sgr0)"
 echo ""
@@ -30,9 +30,9 @@ do
         ## Word3 feature coming soon.
         #WORD3="$(randomWord) $(randomWord) $(randomWord)  $(randomWord) $(randomWord) $(randomWord)"
 
-        echo $N key1 word: $WORD1
+        echo Key1 Put-Record number $N is $WORD1
         aws kinesis put-record --stream-name $STREAM --partition-key 1 --data $WORD1
-        echo $N key2 word: $WORD2
+        echo Key2 Put-Record number $N is $WORD2 
         aws kinesis put-record --stream-name $STREAM --partition-key 2 --data $WORD2 
         ## Word3 feature coming soon.
         #echo $N key2 word: $WORD3
